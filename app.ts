a5dd441348949967a317d82dbd62e50d0e12e9a6
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-02-24 16:10:48 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,6 @@
+let array: (string | number) [] = [`alisha`,`asma`,3,5,8,"tania"]
+console.log(array[5]);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
